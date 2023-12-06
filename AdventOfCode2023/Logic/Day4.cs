681ddb951470584356954cf856871ee51a869422
Geuskens Ben <ben.geuskens@cegeka.com>
2023-12-06 22:00:01 +0100

    Aoc day4 init

---
+++ AdventOfCode2023/Logic/Day4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2023;
+
+public static class Day4
+{
+    private const string Day = "4";
+
+    public static void Execute()
+    {
+        const string exampleLocation = $"./Files/Day{Day}_ex.txt";
+        const string fileLocation = $"./Files/Day{Day}.txt";
+
+        Console.WriteLine(Part1(exampleLocation));
+        Console.WriteLine(Part1(fileLocation));
+
+        Console.WriteLine(Part2(exampleLocation));
+        Console.WriteLine(Part2(fileLocation));
+    }
+
+    public static int Part2(string fileLocation)
+    {
+        return 0;
+    }
+
+
+    public static int Part1(string fileLocation)
+    {
+        return 0;
+    }
+}
---
+++ AdventOfCode2023/Tests/Day4Tests.cs
@@ -0,0 +1,39 @@
+using AdventOfCode2023;
+using FluentAssertions;
+
+namespace Tests;
+
+public class Day4Tests
+{
+    [Fact]
+    public void Part1Ex()
+    {
+        var fileLocation = "./Files/Day4_ex.txt";
+
+        Day4.Part1(fileLocation).Should().Be(13);
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        var fileLocation = "./Files/Day4.txt";
+
+        Day4.Part1(fileLocation).Should().Be(13);
+    }
+
+    [Fact]
+    public void Part2Ex()
+    {
+        var fileLocation = "./Files/Day4_ex.txt";
+
+        Day4.Part2(fileLocation).Should().Be(13);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var fileLocation = "./Files/Day4.txt";
+
+        Day4.Part2(fileLocation).Should().Be(13);
+    }
+}
